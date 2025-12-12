04612f744f6e297a1a9dd4483a2480a5373a952c
Abel Agenjo <abel.agenjo@gmail.com>
2025-12-12 18:51:35 +0100

    Script.js esborrat

--- script.js
+++
@@ -1,7 +0,0 @@
-function hola() {
-    alert("¡Hola mundo!");
-    alert("Hola mundo desde Marta con nuevos user.name y user.mail");
-    alert("Hola mundo 2!);    
-    alert("Modificacio desde MASTER");
-    alert("Modificacio desde DEV");
-}

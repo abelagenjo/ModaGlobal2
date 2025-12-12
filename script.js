6e29db7e64d8284c5372cbffdb69edfac74e70f5
Abel Agenjo <abel.agenjo@gmail.com>
2025-12-12 18:12:20 +0100

    Mod desde MASTER

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function hola() {
     alert("¡Hola mundo!");
     alert("Hola mundo desde Marta con nuevos user.name y user.mail");
-    alert("Hola mundo 2!);
+    alert("Hola mundo 2!);    
+    alert("Modificacio desde MASTER");
 };
